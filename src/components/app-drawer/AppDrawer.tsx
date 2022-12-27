9ddb265dcb14767b3fbcda651904fd456073d00a
JosephJoshua <jj.anggita@gmail.com>
2022-12-27 22:47:30 +0900

    fix: drawer links not.. being actual links

--- src/components/app-drawer/AppDrawer.tsx
+++ src/components/app-drawer/AppDrawer.tsx
@@ -2,19 +2,20 @@ import {
   Accordion,
   Avatar,
   Box,
+  createStyles,
   Group,
   MantineColor,
-  MantineTheme,
   Navbar,
   ScrollArea,
   Stack,
   Text,
   ThemeIcon,
-  UnstyledButton,
   useMantineTheme,
 } from '@mantine/core';
 import { useLocalStorage } from '@mantine/hooks';
 import { TablerIcon } from '@tabler/icons';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React, { FC, useMemo } from 'react';
 
 export type DrawerCategory = {
@@ -26,7 +27,7 @@ export type DrawerCategory = {
 export type DrawerItem = {
   key: React.Key;
   title: string;
-  to?: string;
+  to: string;
   icon: TablerIcon;
   color?: MantineColor;
 };
@@ -35,23 +36,40 @@ export type AppDrawerProps = {
   categories: readonly DrawerCategory[];
 };
 
-const linkButtonStyles = (theme: MantineTheme) => ({
-  display: 'block',
-  width: '100%',
-  padding: `${theme.spacing.xs}px ${theme.spacing.sm}px`,
-  borderRadius: theme.spacing.md,
-  color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
+const useStyles = createStyles((theme) => ({
+  link: {
+    display: 'block',
+    width: '100%',
+    padding: `${theme.spacing.xs}px ${theme.spacing.sm}px`,
+    borderRadius: theme.spacing.md,
+    color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
+    textDecoration: 'none',
 
-  '&:hover': {
+    '&:hover': {
+      backgroundColor:
+        theme.colorScheme === 'dark'
+          ? theme.colors.dark[6]
+          : theme.colors.gray[1],
+    },
+  },
+  activeLink: {
     backgroundColor:
       theme.colorScheme === 'dark'
-        ? theme.colors.dark[6]
-        : theme.colors.gray[0],
+        ? theme.colors[theme.primaryColor][6]
+        : theme.colors[theme.primaryColor][4],
+    color: theme.white,
+
+    '&:hover': {
+      backgroundColor: theme.colors[theme.primaryColor][5],
+    },
   },
-});
+}));
 
 const AppDrawer: FC<AppDrawerProps> = ({ categories }) => {
   const theme = useMantineTheme();
+  const { classes, cx } = useStyles();
+
+  const router = useRouter();
 
   // `useLocalStorage` doesn't allow passing in a function
   // for the `defaultValue` so we have to put this in `useMemo`
@@ -102,21 +120,38 @@ const AppDrawer: FC<AppDrawerProps> = ({ categories }) => {
 
               <Accordion.Panel>
                 <Stack spacing={4}>
-                  {category.children.map((child) => (
-                    <UnstyledButton key={child.key} sx={linkButtonStyles}>
-                      <Group>
-                        <ThemeIcon variant="light" color={child.color}>
-                          {React.createElement(child.icon, {
-                            size: 18,
-                          })}
-                        </ThemeIcon>
+                  {category.children.map((child) => {
+                    const isActive =
+                      router.pathname === child.to ||
+                      router.asPath === child.to;
 
-                        <Text size="sm" weight={500}>
-                          {child.title}
-                        </Text>
-                      </Group>
-                    </UnstyledButton>
-                  ))}
+                    return (
+                      <Box
+                        component={Link}
+                        className={cx(
+                          classes.link,
+                          isActive && classes.activeLink
+                        )}
+                        key={child.key}
+                        href={child.to}
+                      >
+                        <Group>
+                          <ThemeIcon
+                            variant={isActive ? 'filled' : 'light'}
+                            color={isActive ? 'primary.7' : child.color}
+                          >
+                            {React.createElement(child.icon, {
+                              size: 18,
+                            })}
+                          </ThemeIcon>
+
+                          <Text size="sm" weight={500}>
+                            {child.title}
+                          </Text>
+                        </Group>
+                      </Box>
+                    );
+                  })}
                 </Stack>
               </Accordion.Panel>
             </Accordion.Item>
